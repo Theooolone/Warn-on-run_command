0358003488826795eb471b26297c10ff28cd6eae
Theooolone <110533961+Theooolone@users.noreply.github.com>
2025-06-16 18:37:07 +0100

    Removed unused import statement

--- src/main/java/theooolone/warnonruncommand/WarnOnRunCommand.java
+++ src/main/java/theooolone/warnonruncommand/WarnOnRunCommand.java
@@ -3,10 +3,6 @@ package theooolone.warnonruncommand;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class WarnOnRunCommand implements ClientModInitializer {
 	@Override
